e736b80bd96f1f0d4b735ebb4d011bc5ca6eabf9
Bryan <bryan.zamora@goitsa.me>
2020-06-06 09:43:42 -0500

    Funcionalidad del file

--- src/app/nutisurvey/home/encuesta/encuesta.component.ts
+++ src/app/nutisurvey/home/encuesta/encuesta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-encuesta',
@@ -10,6 +11,40 @@ export class EncuestaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('#btn_enviarfile').on('change',function (e){
+      if($(this).val()!=""){
+              $("#txt-enviarfile").css("display","none");
+              $("#btn_enviarfile").css("display","none");
+              $("#btn_eliminarfile").css("display","block");
+              $("#img_logo").css("display","block");
+              $("#txt_value").css("display","block");
+      }
+    });
+
+    $('#btn_eliminarfile').on('click',function (){
+          $("#btn_enviarfile").val("");
+          $("#txt-enviarfile").css("display","block");
+          $("#btn_enviarfile").css("display","block");
+          $("#btn_eliminarfile").css("display","none");
+          $("#img_logo").css("display","none");
+          $("#txt_value").css("display","none");
+    });
   }
+
+  public url:any;
+  onSelectFile(event) { // called each time file input changes
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+        $("#img_logo").attr("src",this.url);
+      }
+    }
+
+    $("#txt_value").text(event.target.files[0].name);
+}
 
 }
