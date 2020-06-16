32c73563f814109f80dabf741e025a0bc7258f70
Bryan <bryan.zamora@goitsa.me>
2020-06-16 18:46:57 -0500

    Olvide contrasena

--- src/app/nutisurvey/nutisurvey-routing.module.ts
+++ src/app/nutisurvey/nutisurvey-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NutisurveyComponent } from './nutisurvey.component';
+import { OlvideContrasenaComponent } from './olvide-contrasena/olvide-contrasena.component';
 
 const routes: Routes = [
   {
      path: '', component: NutisurveyComponent, children: [
       { 
         path: 'login',  component: LoginComponent 
+      },
+      { 
+        path: 'olvide-contraseña',  component: OlvideContrasenaComponent 
       },
       {
         path: 'home' , loadChildren:()=> import('./home/home.module').then(m => m.HomeModule)
